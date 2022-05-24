25569b265c22d6a2b60ee867f1310b72fded3b0e
gilesmaybery <47946129+gilesmaybery@users.noreply.github.com>
2022-05-24 16:13:44 +0200

    click/hover events for list items
    Added some style change when an item is clicked/hovered

--- src/ListItem.jsx
+++ src/ListItem.jsx
@@ -1,8 +1,16 @@
 
 import { Html } from "@react-three/drei";
 import "./styles.css"
+import {useState} from 'react'
 export default function ListItem(props) {
+    const [clicked, click] = useState(false)
+    const [hovered, hover] = useState(false)
     return (
-        <div id="listItem">{props.name}</div>
+        <div 
+        onClick={(evt) => click(!clicked)}
+        onPointerOver={(event) => hover(true)}
+        onPointerOut={(event) => hover(false)}
+        id={hovered ? "listItem-hover" : "listItem"}
+        >{props.name}</div>
     );
 }
--- src/PlanetScene.jsx
+++ src/PlanetScene.jsx
@@ -4,15 +4,15 @@ import Planet from "./Planet";
 function PlanetScene(){
     return(
         <>
-        <Planet name={"Earth"} position={[-40,-1,2]} radius={1} color="green"></Planet>
-        <Planet name={"Jupiter"}position={[-30,-1,2]}radius={5}color="blue"></Planet>
-        <Planet name={"Mars"}position={[-20,-1,2]}radius={0.5}color="red"></Planet>
-        <Planet name={"Mercury"}position={[-10,-1,2]}radius={1.5}color="orange"></Planet>
-        <Planet name={"Neptune"}position={[0,-1,2]}radius={0.8}color="blue"></Planet>
-        <Planet name={"Pluto"}position={[10,-1,2]}radius={0.95}color="darkblue"></Planet>
-        <Planet name={"Saturn"}position={[20,-1,2]}radius={3}color="yellow"></Planet>
-        <Planet name={"Uranus"}position={[30,-1,2]}radius={1.2}color="purple"></Planet>
-        <Planet name={"Venus"}position={[40,-1,2]}radius={0.8}color="orange"></Planet>
+        <Planet name={"Earth"} position={[-20,-1,2]} radius={1} color="green"></Planet>
+        <Planet name={"Jupiter"}position={[-10,-1,2]}radius={5}color="blue"></Planet>
+        <Planet name={"Mars"}position={[0,-1,2]}radius={0.5}color="red"></Planet>
+        <Planet name={"Mercury"}position={[10,-1,2]}radius={1.5}color="orange"></Planet>
+        <Planet name={"Neptune"}position={[20,-1,2]}radius={0.8}color="blue"></Planet>
+        <Planet name={"Pluto"}position={[30,-1,2]}radius={0.95}color="darkblue"></Planet>
+        <Planet name={"Saturn"}position={[40,-1,2]}radius={3}color="yellow"></Planet>
+        <Planet name={"Uranus"}position={[50,-1,2]}radius={1.2}color="purple"></Planet>
+        <Planet name={"Venus"}position={[60,-1,2]}radius={0.8}color="orange"></Planet>
         </>
     )
 }
